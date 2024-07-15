d634d33ef862006f4bd742383a29c7ed04b42299
jciterceros <jciterceros@yahoo.com>
2024-07-15 19:25:32 -0300

    Initial Commit, configure H2

---
+++ src/main/java/com/jciterceros/utidacerveja_backend/UtidacervejaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jciterceros.utidacerveja_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtidacervejaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtidacervejaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jciterceros/utidacerveja_backend/UtidacervejaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jciterceros.utidacerveja_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtidacervejaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
